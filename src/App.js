82f546a89e70cc44f7ede37f8781ea1c89cad037
ztrixack <ztrixack.th@gmail.com>
2021-07-31 00:12:15 +0700

    add style format (eslint, prettier, husky and lint-staged)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import tw from "twin.macro";
+import tw from 'twin.macro'
 
-const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
-const Heading = tw.h1`text-blue-500 text-2xl p-2`;
+const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`
+const Heading = tw.h1`text-blue-500 text-2xl p-2`
 
 function App() {
   return (
     <Container>
       <Heading>Hello world</Heading>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
