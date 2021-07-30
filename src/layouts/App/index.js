ecaad10abb13b041079b81736010441b4fc8ff96
ztrixack <ztrixack.th@gmail.com>
2021-07-31 00:53:16 +0700

    add loading on top

--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -1,14 +1,25 @@
 /** @jsxImportSource @emotion/react */
+import { useRef } from 'react'
 import tw from 'twin.macro'
+import LoadingBar from 'react-top-loading-bar'
 
 const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`
 const Heading = tw.h1`text-blue-500 text-2xl p-2`
+const Button = tw.button`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4`
 
-function App() {
+const App = () => {
+  const ref = useRef(null)
+
   return (
-    <Container>
-      <Heading>Hello world</Heading>
-    </Container>
+    <>
+      <LoadingBar color="#f11946" ref={ref} />
+      <Container>
+        <Heading>Hello world</Heading>
+        <Button onClick={() => ref.current.continuousStart()}>Start Continuous Loading Bar</Button>
+        <Button onClick={() => ref.current.staticStart()}>Start Static Loading Bar</Button>
+        <Button onClick={() => ref.current.complete()}>Complete</Button>
+      </Container>
+    </>
   )
 }
 
